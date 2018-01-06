da50d6836c8adf3923b5e85ccfa1ac44364d55cd
wmzhang <wmzhang@ikuai8.com>
2018-01-06 15:01:17 +0800

    fix buf

---
+++ src/client.cpp
@@ -0,0 +1,53 @@
+#include "client.hpp"
+#include <functional>
+#include <boost/asio/yield.hpp>
+using std::placeholders::_1;
+using boost::system::error_code;
+
+client::client(boost::asio::io_service& io_service, const std::string& address, const std::string& port)
+	: io_service_(io_service),
+	socket_(io_service)
+{
+	boost::system::error_code ec;
+	tcp::resolver resolver(io_service);
+	tcp::resolver::query query(address, port);
+	tcp::resolver::iterator iterator = resolver.resolve(query);
+
+	boost::asio::connect(socket_, iterator);
+}
+
+void client::operator()(error_code ec = error_code(), std::size_t n = 0)
+{
+	if (!ec) reenter(this)
+	{
+		for (;;)
+		{
+			yield boost::asio::async_read(socket_, boost::asio::buffer(auth_message_.header_buffer_),*this);
+			auth_message_.parse_header();
+			yield boost::asio::async_read(socket_, boost::asio::buffer(auth_message_.recv_body_), *this);
+			switch (auth_message_.header_.type_)
+			{
+			case CHECK_CLIENT:
+				auth_message_.parse_check_client_req_msg();
+				auth_message_.constuct_check_client_res_msg();
+				yield boost::asio::async_write(socket_, boost::asio::buffer(auth_message_.send_buffers_), *this);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
+
+
+void client::send_auth_info(const auth_info& msg)
+{
+	io_service_.post(std::bind(&client::do_send_auth_info, this, msg));
+}
+
+void client::do_send_auth_info(const auth_info& msg)
+{
+	auth_message_.constuct_auth_res_msg(msg);
+	boost::asio::async_write(socket_, boost::asio::buffer(auth_message_.send_buffers_),
+		[](const error_code&ec, size_t bytes_transfered) {});
+}
---
+++ src/client.hpp
@@ -0,0 +1,24 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include "auth_message.hpp"
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+class client:boost::asio::coroutine
+{
+public:
+	client(boost::asio::io_service& io_service, const std::string& address, const std::string& port);
+
+	void operator()(boost::system::error_code ec = boost::system::error_code(), std::size_t n = 0);
+
+	void send_auth_info(const auth_info& msg);
+	void do_send_auth_info(const auth_info& msg);
+
+private:
+	boost::asio::io_service& io_service_;
+	auth_message auth_message_;
+	tcp::socket socket_;
+};
+
+#endif
