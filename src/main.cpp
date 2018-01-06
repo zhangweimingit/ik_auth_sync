1c00c8d4e9e53225b14be3ba82b07f84628af48a
wmzhang <wmzhang@ikuai8.com>
2018-01-06 16:14:05 +0800

    fix buf

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,3 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <errno.h>
-#include <string.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-/* According to POSIX.1-2001 */
-#include <sys/select.h>
-
-/* According to earlier standards */
-#include <sys/time.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #include <iostream>
